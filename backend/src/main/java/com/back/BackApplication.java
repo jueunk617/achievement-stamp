320736be8544cccf0953f07191577d270b411e3a
jueunk617 <juuuune62@naver.com>
2025-09-10 14:44:32 +0900

    fix(be): @SpringBootApplication 추가

--- backend/src/main/java/com/back/BackApplication.java
+++ backend/src/main/java/com/back/BackApplication.java
@@ -1,7 +1,11 @@
 package com.back;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BackApplication {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(BackApplication.class, args);
     }
 }
